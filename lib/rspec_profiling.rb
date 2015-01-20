ff00c8203eec09f092211067987712ca4da67a7b
Ben Eddy <bae@foraker.com>
2015-01-19 18:52:53 -0700

    Update rspec_profiling.rb

--- lib/rspec_profiling.rb
+++ lib/rspec_profiling.rb
@@ -2,7 +2,7 @@ require "ostruct"
 require "rails"
 require "rspec_profiling/config"
 require "rspec_profiling/version"
-require "rspec_profiling/profiling"
+require "rspec_profiling/run"
 require "rspec_profiling/collectors/csv"
 require "rspec_profiling/collectors/database"
 

0bccecb64586c422d25ac547a2d99d890b877aa5
Josh Pfosi <joshpfosi@gmail.com>
2015-07-02 11:40:40 -0400

    Added git-svn support

--- lib/rspec_profiling/current_commit.rb
+++ lib/rspec_profiling/current_commit.rb
@@ -4,24 +4,35 @@ module RspecProfiling
   module CurrentCommit
     extend self
 
-    def git?
+    def version_control
+      `[ -d .git/svn  ] && [ x != x"$(ls -A .git/svn/)" ]`
+      return :git_svn if $CHILD_STATUS.success?
+
       `git rev-parse 2>&1`
-      $?.success?
+      return :git if $CHILD_STATUS.success?
+
+      :svn
     end
 
     def sha
-      if git?
+      case version_control
+      when :git
         `git rev-parse HEAD`
-      else
+      when :svn
         `svn info -r 'HEAD' | grep "Revision" | cut -f2 -d' '`
+      when :git_svn
+        `git svn info | grep "Revision" | cut -f2 -d' '`
       end
     end
 
     def time
-      if git?
+      case version_control
+      when :git
         Time.parse `git show -s --format=%ci #{sha}`
-      else
+      when :svn
         Time.parse `svn info -r 'HEAD' | grep "Last Changed Date" | cut -f4,5,6 -d' '`
+      when :git_svn
+        Time.parse `git svn info | grep "Last Changed Date" | cut -f4,5,6 -d' '`
       end
     end
   end
